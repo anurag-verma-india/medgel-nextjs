006f5dd9c6b5194c2a2b5f404e40f1c13b79ed1e
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-02-17 18:38:12 +0530

    Product Category API working

--- src/app/api/product_category/route.ts
+++ src/app/api/product_category/route.ts
@@ -9,14 +9,14 @@ import ProductCategory from "@/models/productCategory";
 
 /*
 No authentication
-Get Categories and list names A
+Get Categories and list names
 
 Email verified check
 Get product details
 
 User is admin check
 PUT, POST, DELETE
- */
+*/
 
 /*
 GET
@@ -66,10 +66,19 @@ export async function GET(request: NextRequest) {
       });
     }
     console.log("Category found: ", category);
-
-    return NextResponse.json({
-      category,
-    });
+    if (category) {
+      return NextResponse.json({
+        success: true,
+        category,
+      });
+    }
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Provided id or name does not exit in the categories list",
+      },
+      { status: 404 },
+    );
   } catch (error) {
     console.error("Error in getting lists from category: ", error); // Log the complete error object
     return new Response(
@@ -81,12 +90,79 @@ export async function GET(request: NextRequest) {
     );
   }
 }
-export async function PUT() {
+export async function PUT(request: NextRequest) {
   // TODO: Make sure the user is admin
+  /*
+  Add products to existing category (identify by id or name)
+  */
+  // No authentication to get categories
+  try {
+    await dbConnect();
+    // const { searchParams } = new URL(request.url);
+    // const product_category_name = searchParams.get("product_category_name");
+    // const product_category_id = searchParams.get("product_category_id");
+
+    const body = await request.json();
+    const { product_category_name, product_category_id, productLists } = body;
+
+    console.log(
+      "product_category_name: ",
+      product_category_name,
+      "product_category_id: ",
+      product_category_id,
+      "productLists: ",
+      productLists,
+    );
+
+    // const category = new ProductCategory({
+    //   product_category_name: product_category_name,
+    //   productLists: product_category_id_list,
+    // });
+
+    // const savedCategory = await category.save();
+    let category;
+    if (product_category_id) {
+      category = await ProductCategory.findById(product_category_id);
+    } else if (product_category_name) {
+      category = await ProductCategory.findOne({
+        product_category_name,
+      });
+    }
+    console.log("Category found: ", category);
+    if (category) {
+      category.productLists = category.productLists.concat(productLists);
+      const savedCategory = await category.save();
+      return NextResponse.json({
+        savedCategory,
+      });
+    }
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          "Provided id or name does not exit in the categories list, send a post request to create a new category",
+      },
+      { status: 404 },
+    );
+  } catch (error) {
+    console.error("Error in getting lists from category: ", error); // Log the complete error object
+    return new Response(
+      JSON.stringify({
+        message: "Failed to get lists from category",
+        error: error.toString(),
+      }),
+      { status: 500 },
+    );
+  }
 }
 
 export async function POST(request: NextRequest) {
   // TODO: Make sure the user is admin
+
+  /* Working
+  Make a new product category
+  (optionally with a list of list_ids)
+  */
   try {
     await dbConnect();
     const body = await request.json();
@@ -113,43 +189,6 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({
       savedCategory,
     });
-    /*
-    If name passed instead of id search that, if not found then add a new one then add product in that category
-     */
-    // if (!product_category_id && !product_category_name) {
-    //   return new Response(
-    //     JSON.stringify({
-    //       message:
-    //         "Please provide either product_category_id or product_category_name",
-    //     }),
-    //     { status: 400 },
-    //   );
-    // }
-
-    // const category = await ProductCategory.findOne({ id: product_category_id });
-    // Try to find the category
-    // let category;
-    // if (product_category_id) {
-    //   category = await ProductCategory.findById(product_category_id);
-    // } else if (product_category_name) {
-    //   category = await ProductCategory.findOne({ name: product_category_name });
-    // }
-
-    // if (!category) {
-    // }
-    // console.log("productLists: ", category.productLists);
-    // // Check if the list id provided in request is present in the category
-    // if (category.productLists.indexOf(product_list_id) === -1) {
-    //   return new Response(
-    //     JSON.stringify({
-    //       message:
-    //         "The product id you provided is not present in the product list",
-    //     }),
-    //     { status: 400 },
-    //   );
-    // }
-
-    // const list = await ProductList.findById(product_list_id);
   } catch (error) {
     console.error("Error in adding product: ", error); // Log the complete error object
     return new Response(
@@ -161,77 +200,3 @@ export async function POST(request: NextRequest) {
     );
   }
 }
-
-// export async function PUT(request: NextRequest) {
-//   /*
-//   Add new lists or products to product categories
-//    */
-//   try {
-//     await dbConnect();
-//     const body = await request.json();
-
-//     const {
-//       product_category_id,
-//       product_category_name,
-//       product_list_id,
-//       product_details_arr,
-//     } = body;
-
-//     console.log(
-//       "product_category_id: ",
-//       product_category_id,
-//       "product_category_name: ",
-//       product_category_name,
-//       "product_list_id: ",
-//       product_list_id,
-//       "product_details_arr: ",
-//       product_details_arr,
-//     );
-//     /*
-//     If name passed instead of id search that, if not found then add a new one then add product in that category
-//      */
-//     if (!product_category_id && !product_category_name) {
-//       return new Response(
-//         JSON.stringify({
-//           message:
-//             "Please provide either product_category_id or product_category_name",
-//         }),
-//         { status: 400 },
-//       );
-//     }
-
-//     // const category = await ProductCategory.findOne({ id: product_category_id });
-//     // Try to find the category
-//     let category;
-//     if (product_category_id) {
-//       category = await ProductCategory.findById(product_category_id);
-//     } else if (product_category_name) {
-//       category = await ProductCategory.findOne({ name: product_category_name });
-//     }
-
-//     if (!category) {
-//     }
-//     console.log("productLists: ", category.productLists);
-//     // Check if the list id provided in request is present in the category
-//     if (category.productLists.indexOf(product_list_id) === -1) {
-//       return new Response(
-//         JSON.stringify({
-//           message:
-//             "The product id you provided is not present in the product list",
-//         }),
-//         { status: 400 },
-//       );
-//     }
-
-//     // const list = await ProductList.findById(product_list_id);
-//   } catch (error) {
-//     console.error("Error in editing product: ", error); // Log the complete error object
-//     return new Response(
-//       JSON.stringify({
-//         message: "Failed to put product",
-//         error: error.toString(),
-//       }),
-//       { status: 500 },
-//     );
-//   }
-// }
