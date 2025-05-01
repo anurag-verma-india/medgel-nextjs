45d94b5dfc5df406e43a3ab55acbb1dff97672bb
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-05-01 18:28:04 +0530

    Fixed a typescript error

--- src/app/_common_component/EditImages.tsx
+++ src/app/_common_component/EditImages.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import React from "react";
 
 type EditImagesParams = {
-  images: [ImageObj];
+  images: ImageObj[];
 };
 
 const EditImages = ({ images }: EditImagesParams) => {
