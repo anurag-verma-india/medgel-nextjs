3d46af9f8aad52216cbd45f49864dc09b5b114b7
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-05-19 18:16:44 +0530

    Moved Home page.tsx to to base directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-    return <div>Hello world</div>;
-}
+import SliderComponent from "./home/slider";
+import CardRow from "./home/CardRow";
+import PurposeSection from "./home/PurposeSection";
+import Bpurpose from "./home/Bpurpose";
+import Cpurpose from "./home/Cpurpose";
+// import Header from "./Header";
+// import Footer from "./Footer";
+import Portfolio from "./home/Portfolio";
+import HomeNews from "./home/homenews";
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* <Header /> */}
+      <SliderComponent />
+      <CardRow />
+      <PurposeSection />
+      <Bpurpose />
+      <HomeNews />
+      {/* <LatestAtMedgel2/> */}
+      {/* <LatestAtMedgel/> */}
+      <Portfolio />
+      <Cpurpose />
+
+      {/* <Footer />  */}
+    </div>
+  );
+};
+
+export default HomePage;
