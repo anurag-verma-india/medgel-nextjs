02628ba566e3f0c069d235ad072ab55e75c19c25
Taha <110016145+Tahacollege@users.noreply.github.com>
2025-06-12 12:25:16 +0530

    Page Edit popup: z index css modified

--- src/app/_common_component/EditModalContainer.tsx
+++ src/app/_common_component/EditModalContainer.tsx
@@ -106,7 +106,7 @@ export default function EditModalContainer({ title }: { title: string }) {
       <div className="relative">
         <button
           onClick={onClick}
-          className="mr-5 mt-5 absolute right-0 top-0 rounded bg-[#00a5a5] px-4 py-2 text-black opacity-40 shadow hover:bg-[#197777] focus:outline-none focus:ring-2 focus:ring-black"
+          className="mr-5 mt-5 absolute  z-50 right-0 top-0 rounded bg-[#00a5a5] px-4 py-2 text-black opacity-40 shadow hover:bg-[#197777] focus:outline-none focus:ring-2 focus:ring-black"
         >
           Edit
         </button>
