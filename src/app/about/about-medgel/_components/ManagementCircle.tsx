10a39d040898100c11d35c195c9e28205eec28e9
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-06-12 11:21:22 +0530

    Merge branch 'rajat' into test-build

--- src/app/about/about-medgel/_components/ManagementCircle.tsx
+++ src/app/about/about-medgel/_components/ManagementCircle.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import "./ManagementCircle.css";
 
@@ -39,6 +40,67 @@ const ManagementCircle = () => {
             <p>{person.title}</p>
           </div>
         ))}
+=======
+import React, { ReactNode } from "react";
+import "./ManagementCircle.css";
+
+const topManagement = [
+  {
+    name: "Mr. R C Mittal",
+    title: "Chairman",
+    img: "/aboutUsImg/img2.png",
+  },
+  {
+    name: "Mr. Alok K.Garg",
+    title: "Vice-Chairman",
+    img: "/aboutUsImg/img3.png",
+  },
+];
+
+const lowerManagement = [
+  {
+    name: "Dr. Shashi Kant Sharma Verma",
+    title: "Director",
+    img: "/aboutUsImg/img3.png",
+  },
+  {
+    name: "Mr. Praveen Nalwaya",
+    title: "Director",
+    img: "/aboutUsImg/img1.png",
+  },
+];
+
+const ManagementCircle = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: ReactNode;
+}) => {
+  return (
+    <>
+      {children}
+      <div className="management-container">
+        <h1 className="title">Management Circle</h1>
+        <div className="management-row">
+          {topManagement.map((person, index) => (
+            <div key={index} className="management-card raised-card">
+              <img src={person.img} alt={person.name} />
+              <h2>{person.name}</h2>
+              <p>{person.title}</p>
+            </div>
+          ))}
+        </div>
+        <div className="lower-management">
+          {lowerManagement.map((person, index) => (
+            <div key={index} className="management-card">
+              <img src={person.img} alt={person.name} />
+              <h2>{person.name}</h2>
+              <p>{person.title}</p>
+            </div>
+          ))}
+        </div>
+>>>>>>> 341b9a9 (Merge branch 'rajat' into test-build)
       </div>
       <div className="lower-management">
         {lowerManagement.map((person, index) => (
