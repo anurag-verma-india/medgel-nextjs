de6e023f480ba1677ce4e0b5b94f831f9af292c5
rajat <agrawal21rajat@gmail.com>
2025-06-16 16:44:47 +0530

    updated the Footer

--- src/app/_common_component/Footer.tsx
+++ src/app/_common_component/Footer.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./Footer.css";
 import Link from "next/link";
@@ -13,18 +12,54 @@ const Footer = () => {
           </Link>JS, CSS and
  JS, CSS anudiv> */}
         <div className="footer-column">
-          <h4>About Us</h4>
-          <ul>
-            <Link href={"/about/life-at-medgel"}>
+          <div className="footer-logo">
+            <img src="medgel-logo.svg" alt="medgel-logo" />
+          </div>
+          <h4>Contact Details</h4>
+          <ul className="md:text-left">
+            {/* <Link href={"/about/life-at-medgel"}>
               <li>Life At Medgel</li>
             </Link>
             <Link href={"/about/awards_"}>
               <li>Awards & Accreditation</li>
+            </Link> */}
+
+            <li>
+              <span className="pr-3 text-xl font-bold">Phone: </span>
+              +91-07292-256205
+            </li>
+            <li>
+              <span className="invisible pr-3 text-xl font-bold">Phone: </span>
+              +91-07292-256205
+            </li>
+            <li>
+              <span className="pr-3 text-xl font-bold">Email: </span>
+              <span className="text-[15px] md:text-[1.1rem]">
+                marketing@medgel.net
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-column pl-5">
+          <h4>Quick links</h4>
+          <ul className="text-2xl underline">
+            <Link href={"/products/products-at-medgel"}>
+              <li>Home</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Facilities</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Investor Relations</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Quality & Compliance</li>
             </Link>
           </ul>
         </div>
 
-        <div className="footer-column">
+        {/* <div className="footer-column">
           <h4>Products</h4>
           <ul>
             <Link href={"/products/products-at-medgel"}>
@@ -34,17 +69,49 @@ const Footer = () => {
               <li>About Medgel</li>
             </Link>
           </ul>
-        </div>
+        </div> */}
 
-        <div className="footer-column">
+        {/* <div className="footer-column">
           <Link href={"/facilities"}>
-            <h4>Facilities</h4>
+            <h4>About Us </h4>
           </Link>
         </div>
         <div className="footer-column">
           <Link href={"/contact"}>
             <h4>Contact us</h4>
           </Link>
+        </div> */}
+
+        {/* <ul className="text-2xl underline">
+            <Link href={"/products/products-at-medgel"}>
+              <li>Home</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Facilities</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Investor Relations</li>
+            </Link>
+            <Link href={"/products/medgel-services"}>
+              <li>Quality & Compliance</li>
+            </Link>
+          </ul> */}
+
+        <div className="footer-column">
+          <h4>About Us</h4>
+          <ul className="list-disc pl-8 underline">
+            <li>About Medgel</li>
+            <li>Awards and Accrediation</li>
+            <li>Life at Medgel</li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>Products</h4>
+          <ul className="list-disc pl-8 underline">
+            <li>Products at Medgel</li>
+            <li>Medgel Services</li>
+          </ul>
         </div>
       </div>
       <div className="footer-bottom">
