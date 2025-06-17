62087ca8850640e2858f7fe9e9a69f21a1022305
anurag-verma-india <104461155+anurag-verma-india@users.noreply.github.com>
2025-06-17 15:38:01 +0530

    Next config: Added all dev environments

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    logging: {
-        fetches: {
-            fullUrl: true,
-        },
+  /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
     },
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            // {
-            //     hostname: "example.com",
-            // },
-            {
-                hostname: "localhost",
-            },
-        ],
-    },
+  },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      // {
+      //     hostname: "example.com",
+      // },
+      {
+        hostname: "localhost",
+      },
+      {
+        hostname: "10.10.16.243",
+      },
+      {
+        hostname: "pika-welcome-cicada.ngrok-free.app",
+      },
+    ],
+  },
+  // Fix for cross-origin request warning
+  allowedDevOrigins: [
+    "localhost",
+    "10.10.16.243",
+    "pika-welcome-cicada.ngrok-free.app",
+  ],
 };
 
 export default nextConfig;
